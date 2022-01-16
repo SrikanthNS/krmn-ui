884a9832bdc0f29346a81bbcca49ffdacd411b9f
Srikanth N S <7464926+SrikanthNS@users.noreply.github.com>
2022-01-16 16:01:40 +0530

    UI changesd

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'api',
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:6868/api',
     headers: {
         "Content-type": "application/json"
     }
